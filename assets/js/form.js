24bfbb2be2b0f3db24f60fcd2e8bebc58bdfdfb3
Mya Weigandt <myaweigandt@Myas-MacBook-Air.local>
2024-06-08 21:12:57 -0400

    usuername working

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,8 @@
-alert("form js linked")
+const submitBtn = document.getElementById("submit-button");
+submitBtn.addEventListener("click", getUserName);
+
+function getUserName() {
+  let username = document.getElementById("text1").value;
+  console.log(username);
+  document.getElementById("name").innerHTML = username;
+}
